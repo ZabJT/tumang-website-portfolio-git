fa5cb6ef08fdc88d55b0f3b1666a962e6659de76
ZabJT <zabdieljairus@gmail.com>
2025-06-28 15:14:50 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,6 +17,7 @@ import { useTheme } from "@mui/material/styles";
 const navLinks = [
   { label: "Home", id: "home" },
   { label: "About", id: "about" },
+  { label: "Experience", id: "experience" },
   { label: "Projects", id: "projects" },
   { label: "Contact", id: "contact" },
 ];
